2ae16cf761cf3638ffcfe4b0fe8879c89350f842
lujo983 <lujoquintine4@gmail.com>
2026-04-18 16:16:12 +0300

    Update modified_dashboard.py

--- modified_dashboard.py
+++ modified_dashboard.py
@@ -182,10 +182,10 @@ if menu == "Register":
 if menu == "Login" and not st.session_state.get("logged_in", False):
     st.subheader("Login to BM-SYSTEM Dashboard\n- Home of Entrepreneurs\n- BIASHARA YAKO MKONONI MWAKO")
     # Create 3 columns with ratios (adjust ratios for different image sizes)
-    col1, col2, col3 = st.columns([1, 1, 1])
+    col1, col2, col3 = st.columns([0.5, 1, 0.5])
     
     with col2:
-        st.image("bridge gap tra.jpg, style="border-radius: 59%"")
+        st.image("bridge gap tra.jpg")
     
     with st.form("login_form"):
         email = st.text_input("Email Address")
