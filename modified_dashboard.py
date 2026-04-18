e3d04d57a870d79dd0b6d3d3707ec1e10b1bd11f
lujo983 <lujoquintine4@gmail.com>
2026-04-18 16:47:42 +0300

    Update modified_dashboard.py

--- modified_dashboard.py
+++ modified_dashboard.py
@@ -121,7 +121,7 @@ if 'logged_in' not in st.session_state:
     st.session_state.role = ""
 
 # Sidebar Navigation
-st.sidebar.image("bm.png", width=50)
+st.sidebar.image("bm.png", width=100)
 st.sidebar.title("Navigation")
 
 # Conditional menu
@@ -185,7 +185,7 @@ if menu == "Login" and not st.session_state.get("logged_in", False):
     col1, col2, col3 = st.columns([0.5, 0.5, 0.5])
     
     with col2:
-        st.image("bm.png")
+        st.image("bm.png", width=100)
     
     with st.form("login_form"):
         email = st.text_input("Email Address")
