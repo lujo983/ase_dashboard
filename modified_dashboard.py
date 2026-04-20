34f7b73a245d4ee24b6b798e7132f717046b0a9a
lujo983 <lujoquintine4@gmail.com>
2026-04-20 15:08:57 +0300

    Update modified_dashboard.py

--- modified_dashboard.py
+++ modified_dashboard.py
@@ -727,7 +727,7 @@ if st.session_state.logged_in and menu == "Dashboard":
                                  # Summary Row in PDF
                                  pdf_data.append(["TOTAL", "", "", "", "", f"Tsh {sales:,.0f}", f"Loss: {total_loss:,.0f}"])
          
-                                 t = Table(pdf_data, colWidths=[0.6*inch, 1.2*inch, 1.5*inch, 0.5*inch, 0.9*inch, 1.1*inch, 1.1*inch])
+                                 t = Table(pdf_data, colWidths=[0.6*inch, 1.2*inch, 1.3*inch, 0.5*inch, 0.9*inch, 1.1*inch, 1.1*inch])
                                  t.setStyle(TableStyle([
                                      ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor("#1E3A8A")),
                                      ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
