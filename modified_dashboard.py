1ffaf5b63a5082ad096bc7ff4759fb09f8e54bd2
lujo983 <lujoquintine4@gmail.com>
2026-04-20 12:00:49 +0300

    Update modified_dashboard.py

--- modified_dashboard.py
+++ modified_dashboard.py
@@ -185,7 +185,7 @@ if menu == "Login" and not st.session_state.get("logged_in", False):
     col1, col2, col3 = st.columns([0.5, 0.5, 0.5])
     
     with col2:
-        st.image("bridge gap tra.jpg", width=100)
+        st.image("bridge gap tra.jpg", width=200)
     
     with st.form("login_form"):
         email = st.text_input("Email Address")
