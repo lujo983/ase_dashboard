13cfd968fc538d58fafff432f98e26b6b3814d90
lujo983 <lujoquintine4@gmail.com>
2026-04-20 12:02:09 +0300

    Update modified_dashboard.py

--- modified_dashboard.py
+++ modified_dashboard.py
@@ -121,7 +121,7 @@ if 'logged_in' not in st.session_state:
     st.session_state.role = ""
 
 # Sidebar Navigation
-st.sidebar.image("bridge gap tra.jpg", width=100)
+st.sidebar.image("bridge gap tra.jpg", width=200)
 st.sidebar.title("Navigation")
 
 # Conditional menu
@@ -185,7 +185,7 @@ if menu == "Login" and not st.session_state.get("logged_in", False):
     col1, col2, col3 = st.columns([0.5, 0.5, 0.5])
     
     with col2:
-        st.image("bridge gap tra.jpg", width=200)
+        st.image("bridge gap tra.jpg", width=300)
     
     with st.form("login_form"):
         email = st.text_input("Email Address")
