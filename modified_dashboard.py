9cd4dce216e7f84a99c8583b658c89fc08032ab7
lujo983 <lujoquintine4@gmail.com>
2026-04-21 22:24:19 +0300

    Update modified_dashboard.py

--- modified_dashboard.py
+++ modified_dashboard.py
@@ -204,7 +204,7 @@ if menu == "Login" and not st.session_state.get("logged_in", False):
     col1, col2, col3 = st.columns([0.5, 0.5, 0.5])
     
     with col2:
-        st.image("bm_logo.png", width=200)
+        st.image("bm_logo.png", width=300)
     st.subheader("Login to the SYSTEM.\n- Home of Entrepreneurs\n- BIASHARA YAKO MKONONI MWAKO")
     with st.form("login_form"):
         email = st.text_input("Email Address")
