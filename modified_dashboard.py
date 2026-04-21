12df998b6b09ef3e9e7a822ac77e41d5728173bb
lujo983 <lujoquintine4@gmail.com>
2026-04-21 22:19:59 +0300

    Update modified_dashboard.py

--- modified_dashboard.py
+++ modified_dashboard.py
@@ -200,13 +200,12 @@ if menu == "Register":
 
 # LOGIN FORM
 if menu == "Login" and not st.session_state.get("logged_in", False):
-    st.subheader("Login to BM-SYSTEM Dashboard\n- Home of Entrepreneurs\n- BIASHARA YAKO MKONONI MWAKO")
     # Create 3 columns with ratios (adjust ratios for different image sizes)
     col1, col2, col3 = st.columns([0.5, 0.5, 0.5])
     
     with col2:
         st.image("bm_logo.png", width=300)
-    
+    st.subheader("Login to BM-SYSTEM.\n- Home of Entrepreneurs\n- BIASHARA YAKO MKONONI MWAKO")
     with st.form("login_form"):
         email = st.text_input("Email Address")
         password = st.text_input("Password", type="password")
