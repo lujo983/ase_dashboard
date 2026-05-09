17df2a901246d5c339a71449db699c11fd120d55
lujo983 <lujoquintine4@gmail.com>
2026-05-09 17:52:24 +0300

    Update 4_communitymember.py

--- pages/4_communitymember.py
+++ pages/4_communitymember.py
@@ -696,8 +696,7 @@ if st.session_state.logged_in and menu == "Dashboard":
                     elif filter_muda == "💸 Marejesho":
                         st.subheader("💸 Marejesho")
                     elif filter_muda == "💡 Ushauri":
-                        st.subheader("🤔 ⚖️💡 USHAURI WA BIASHARA YAKO")
-                        st.header("Ripoti ya Matumizi")
+                        st.header("📊 Ripoti ya Matumizi")
                         # Example: Admins see all, Staff see only theirs
                         user_role = st.session_state.get("role")
                         user_id = st.session_state.get("user_id")
